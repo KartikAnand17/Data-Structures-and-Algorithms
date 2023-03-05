00a5a184a3719eecf5219582390061df00932a38
KartikAnand17 <63124625+KartikAnand17@users.noreply.github.com>
2023-03-05 18:40:23 +0530

    Create good_pairs.cpp

---
+++ Leetcode/good_pairs.cpp
@@ -0,0 +1,16 @@
+//Problem Satement -> https://leetcode.com/problems/number-of-good-pairs/
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int total = 0;
+        for(int i = 0; i< nums.size(); i++){
+            for (int j = i+1; j< nums.size(); j++){
+                if(nums[i] == nums[j]){
+                    total = total + 1;
+                }
+            }
+        } 
+        return total;     
+    }
+};
