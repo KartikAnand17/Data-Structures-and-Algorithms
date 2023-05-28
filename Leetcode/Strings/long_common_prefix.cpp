961228dc60e167dc93f814e5e031e593854edc33
KartikAnand17 <63124625+KartikAnand17@users.noreply.github.com>
2023-05-28 15:43:00 +0530

    Create long_common_prefix.cpp

---
+++ Leetcode/Strings/long_common_prefix.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/longest-common-prefix/
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans = "";
+        sort(strs.begin(),strs.end());
+        string first = strs[0];
+        string last = strs[strs.size() - 1];
+        for(int i = 0; i<min(first.size(),last.size()); i++)
+        {
+            if(first[i] != last[i])
+            {
+                return ans;
+            }
+            else
+            {
+                ans += first[i];
+            }
+        }
+        return ans;
+    }
+};
