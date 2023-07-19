4914dd14f6d5461344ff622ca1f4616e973c5d50
KartikAnand17 <63124625+KartikAnand17@users.noreply.github.com>
2023-07-19 15:14:27 +0530

    Create parentheses.cpp

---
+++ Leetcode/Stacks/parentheses.cpp
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/valid-parentheses/description/
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.length();i++){
+            char ch = s[i];
+            // opening bracket
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }
+            // closing bracket
+            else{
+                if(!st.empty()){
+                    char topCh = st.top();
+                    if(ch == ')' && topCh == '('){
+                        st.pop();
+                    }
+                    else if(ch == '}' && topCh == '{'){
+                        st.pop();
+                    }
+                    else if(ch == ']' && topCh == '['){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(st.empty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
